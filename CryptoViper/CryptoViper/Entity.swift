69f205e2c959be1b614fc4b8710907c9cb4e8d87
ONUR ŞAHİN <develonur@gmail.com>
2025-01-08 14:03:37 +0300

    add: Crypto Viper

---
+++ CryptoViper/CryptoViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Onur Şahin on 8.01.2025.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
