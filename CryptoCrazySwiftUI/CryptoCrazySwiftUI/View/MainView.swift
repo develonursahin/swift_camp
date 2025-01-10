2e5d1d29095e4da2a55ca4231fd745e7b65a5717
ONUR ŞAHİN <develonur@gmail.com>
2025-01-10 09:31:40 +0300

    edit: CryptoCrazySwiftUI (Threading, Async, Await)

--- CryptoCrazySwiftUI/CryptoCrazySwiftUI/View/MainView.swift
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUI/View/MainView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct MainView: View {
+    @ObservedObject var cryptoListViewModel : CryptoListViewModel
+    init() {
+        self.cryptoListViewModel = CryptoListViewModel()
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            
+                List(cryptoListViewModel.cryptoList, id:\.id) { crypto in
+                    VStack{
+                        Text(crypto.currency ?? "")
+                        .font(.title3)
+                        .foregroundColor(.red)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(crypto.price ?? "")
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            
+                }.toolbar(content: {
+                    Button{
+                        Task.init {
+//                            cryptoListViewModel.cryptoList = []
+                             await   cryptoListViewModel.downloadCryptosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+                        }
+                    } label: {
+                         Text("Refresh")
+                    }
+                })
+                .navigationTitle(Text("Crypto Crazy"))
+    }
+        .task {
+            await   cryptoListViewModel.downloadCryptosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+//         await   cryptoListViewModel.downloadCryptosAsync(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
         }
-        .padding()
+        /*
+        .onAppear{
+        cryptoListViewModel.downloadCrypto(url:     URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+    }
+        */
     }
 }
 
